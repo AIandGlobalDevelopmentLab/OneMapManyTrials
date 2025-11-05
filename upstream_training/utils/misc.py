aeaf8ebb80320745cc1831e1c5c9c84a185f11fb
mackzoor <markus.pettersson97@gmail.com>
2025-11-05 14:19:37 +0100

    Fix intendation

--- upstream_training/utils/misc.py
+++ upstream_training/utils/misc.py
@@ -25,8 +25,8 @@ def get_save_dir_name(args):
     return '_'.join(parts)
 
 def score_function(y, kde, delta=1e-5):
-        # Derivative of log density
-        log_p_plus = kde.logpdf(y + delta)[0]
-        log_p_minus = kde.logpdf(y - delta)[0]
-        d_logp = (log_p_plus - log_p_minus) / (2 * delta)
-        return d_logp
+    # Derivative of log density
+    log_p_plus = kde.logpdf(y + delta)[0]
+    log_p_minus = kde.logpdf(y - delta)[0]
+    d_logp = (log_p_plus - log_p_minus) / (2 * delta)
+    return d_logp
